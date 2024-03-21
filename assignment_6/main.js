20604a9a87ea69d63eaaceef8d83a6498b70eea7
MasoodNoor <themasoodnoor100@gmail.com>
2024-03-22 02:04:31 +0500

    Assignment 6 completed

---
+++ assignment_6/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let person_name = "\n\t Masood Noor Rehmat \n\t";
+console.log(person_name);
+let person_name_with_nospace = person_name.trim();
+console.log(`Trimmed name: ${person_name_with_nospace}`);
---
+++ assignment_6/main.ts
@@ -0,0 +1,5 @@
+let person_name: string = "\n\t Masood Noor Rehmat \n\t";
+console.log(person_name);
+
+let person_name_with_nospace : string = person_name.trim();
+console.log(`Trimmed name: ${person_name_with_nospace}`)
