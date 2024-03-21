4800d0a259a49101e82b80caa5723c77a7ef026f
MasoodNoor <themasoodnoor100@gmail.com>
2024-03-22 01:17:51 +0500

    completed assignment 1

---
+++ assignment_1/main.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("Hello world!, from TypeScript & Nodejs project");
+console.log("--------------------------------");
+console.log("Aiming to complete 45 Exercises with TypeScript and Node.js");
---
+++ assignment_1/main.ts
@@ -0,0 +1,4 @@
+console.log("Hello world!, from TypeScript & Nodejs project");
+console.log("--------------------------------");
+console.log("Aiming to complete 45 Exercises with TypeScript and Node.js");
+
