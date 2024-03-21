8b2ad16df3899ba92e38d06b5f9c1c62b2d805e3
MasoodNoor <themasoodnoor100@gmail.com>
2024-03-22 02:01:50 +0500

    Assignment 5 completed

---
+++ assignment_5/main.js
@@ -0,0 +1,5 @@
+"use strict";
+const famous_person = "Abdul Qadeer Khan";
+const qoute = "Science knows no country, because knowledge belongs to humanity, and is the torch which illuminates the world.";
+const message = `${famous_person} once said,"${qoute}"`;
+console.log(message);
---
+++ assignment_5/main.ts
@@ -0,0 +1,6 @@
+const famous_person: string = "Abdul Qadeer Khan";
+const qoute: string =
+    "Science knows no country, because knowledge belongs to humanity, and is the torch which illuminates the world.";
+const message = `${famous_person} once said,"${qoute}"`
+
+console.log(message);
