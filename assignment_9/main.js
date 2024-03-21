b4f608105982bbab784435f9cab5354e5ed5bd56
MasoodNoor <themasoodnoor100@gmail.com>
2024-03-22 02:10:54 +0500

    Assignment 9 completed

---
+++ assignment_9/main.js
@@ -0,0 +1,4 @@
+"use strict";
+let favNumber = 17;
+let message = `Here is my favorite number ${favNumber}.`;
+console.log(message);
---
+++ assignment_9/main.ts
@@ -0,0 +1,3 @@
+let favNumber: number = 17
+let message: string = `Here is my favorite number ${favNumber}.`
+console.log(message);
