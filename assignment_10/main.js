cdc97a74278812d7b8290947c2574b75c0a36738
MasoodNoor <themasoodnoor100@gmail.com>
2024-03-22 02:18:26 +0500

    Assignmnet 10 completed

---
+++ assignment_10/main.js
@@ -0,0 +1,23 @@
+"use strict";
+// Name: { Masood Noor }
+// Date: {7 March 2024}
+// The below code belongs to Task 7
+// In this we use mathematical operators + , - , / , *  to get output result as number 8.
+console.log('=====\t This is the result of exercise 8 \t=====');
+let addition = 4 + 4;
+console.log(`Addition result:\t ${addition}`);
+let subtraction = 12 - 4;
+console.log(`Subtraction result:\t ${subtraction}`);
+let multiplaction = 8 * 1;
+console.log(`Multiplaction result:\t ${multiplaction}`);
+let division = 24 / 3;
+console.log(`Division result:\t ${division}`);
+console.log(`================================================================`);
+console.log('=====\t This is the result of exercise 4 \t=====');
+// This below code belongs to Task 4
+// In this code we store famous person name and its qoute in const variable and then concat it togeather in an other varibale
+const famous_person = "Abdul Qadeer Khan";
+const qoute = `Science knows no country, because knowledge belongs to humanity,
+   and is the torch which illuminates the world.`;
+const message = `${famous_person} once said,"${qoute}"`;
+console.log(message);
