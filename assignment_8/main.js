b8e517135424a1a0013a63a2879bc3b9d95efaf4
MasoodNoor <themasoodnoor100@gmail.com>
2024-03-22 02:08:21 +0500

    Assignment 8 completed

---
+++ assignment_8/main.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log(4 + 4);
+console.log(12 - 4);
+console.log(8 * 1);
+console.log(24 / 3);
---
+++ assignment_8/main.ts
@@ -0,0 +1,4 @@
+console.log(4 + 4);
+console.log(12 - 4);
+console.log(8 * 1);
+console.log(24 / 3);
