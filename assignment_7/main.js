7d36235fc3eeddf9f283fa4bbb1ee9881cf15595
MasoodNoor <themasoodnoor100@gmail.com>
2024-03-22 02:06:10 +0500

    Assignment 7 completed

---
+++ assignment_7/main.js
@@ -0,0 +1,9 @@
+"use strict";
+let addition = 4 + 4;
+console.log(`Addition result:\t ${addition}`);
+let subtraction = 12 - 4;
+console.log(`Subtraction result:\t ${subtraction}`);
+let multiplaction = 8 * 1;
+console.log(`Multiplaction result:\t ${multiplaction}`);
+let division = 24 / 3;
+console.log(`Division result:\t ${division}`);
---
+++ assignment_7/main.ts
@@ -0,0 +1,12 @@
+
+let addition: number = 4 + 4
+console.log(`Addition result:\t ${addition}`)
+
+let subtraction: number = 12 - 4
+console.log(`Subtraction result:\t ${subtraction}`)
+
+let multiplaction: number = 8 * 1
+console.log(`Multiplaction result:\t ${multiplaction}`)
+
+let division: number = 24 / 3
+console.log(`Division result:\t ${division}`)
