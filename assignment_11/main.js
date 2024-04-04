ed63821a9426cd0eb853848433a33dd4ca4cee69
MasoodNoor <themasoodnoor100@gmail.com>
2024-04-05 02:29:27 +0500

    assignmet 11 complete

---
+++ assignment_11/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let names = ["Karar", "Saad", "Arshad", "Sammi", "Rahees", "Yunus"];
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i]);
+// }
+names.forEach(name => {
+    console.log(name);
+});
---
+++ assignment_11/main.ts
@@ -0,0 +1,11 @@
+let names: string[] = ["Karar", "Saad", "Arshad", "Sammi", "Rahees", "Yunus"];
+
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i]);
+
+// }
+
+names.forEach(name => {
+    console.log(name);
+    
+});
