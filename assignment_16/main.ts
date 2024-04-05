b190e6aba4a6dc65176370a96fa8ff0510ead4a1
MasoodNoor <themasoodnoor100@gmail.com>
2024-04-06 04:01:20 +0500

    Assignmnet 16 completed

---
+++ assignment_16/main.ts
@@ -0,0 +1,19 @@
+let guestlist: string[] = ["Rameez", "Shahjahan", "Arshad","Shazaib"]
+// console.log(guestlist);
+
+console.log("Great News! we have now more space available, we have a bigger table. ");
+
+// adding new guest in list at beginning using unshift function
+
+guestlist.unshift("Muhannad")
+// console.log(guestlist);
+
+// adding new guest in list at middle using splice function
+guestlist.splice(Math.floor(guestlist.length/2), 0, "Karar")
+//  console.log(guestlist);
+
+// adding new guest in list at end using push function
+guestlist.push("Saad")
+guestlist.forEach(myGuestList => {
+    console.log(`Dear ${myGuestList}, You all are cordially invited to dinner.`);
+});
