98d6f9b2f54e991a81802cd97b47cc9754720dc2
MasoodNoor <themasoodnoor100@gmail.com>
2024-04-05 05:16:56 +0500

    assignment 14 completed

---
+++ assignment_14/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let myGuest = ["Rameez", "Aslam", "Shahjahan", "Shazaib"];
+for (let i = 0; i < myGuest.length; i++) {
+    console.log(`Dear ${myGuest[i]}, you are cordially invited to dinner.`);
+}
---
+++ assignment_14/main.ts
@@ -0,0 +1,5 @@
+let myGuest: string[] = ["Rameez", "Aslam", "Shahjahan", "Shazaib"]
+
+for (let i = 0; i < myGuest.length; i++) {
+    console.log(`Dear ${myGuest[i]}, you are cordially invited to dinner.`);
+}
