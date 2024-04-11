033b81733f902c68f8dfe388e5a9b77c5b3b3d61
MasoodNoor <themasoodnoor100@gmail.com>
2024-04-12 01:44:33 +0500

    Assignment 20 complete

---
+++ assignment_20/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let operaingSystem = ["Windows", "MacOS", "iOS", "Debian", "Ubuntu"];
+console.log("List of Operating System:");
+operaingSystem.forEach(os => {
+    console.log(os);
+});
---
+++ assignment_20/main.ts
@@ -0,0 +1,6 @@
+let operaingSystem: string[] = ["Windows", "MacOS", "iOS", "Debian", "Ubuntu"]
+console.log("List of Operating System:")
+
+operaingSystem.forEach(os => {
+    console.log(os);
+      });
