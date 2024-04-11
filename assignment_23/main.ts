e42fd95576a63596b1fe6fbf830eaa0c50c33612
MasoodNoor <themasoodnoor100@gmail.com>
2024-04-12 04:51:29 +0500

    Assignment 23 complete

---
+++ assignment_23/main.ts
@@ -0,0 +1,40 @@
+// test #1
+let car = "subaru"
+console.log("Is car == 'subaru' ? I predict True.");
+console.log(car == "subaru");
+
+// test #2
+console.log("Is car != 'subaru' ? I predict False.");
+console.log(car != "subaru");
+
+// test #3
+console.log("Is car == 'Meran' ? I predict False.");
+console.log(car == "Meran");
+
+// test #4
+console.log("Is car === 'subaru' ? I predict True.");
+console.log(car === "subaru");
+
+// test #5
+console.log("Is car !== 'subaru' ? I predict False.");
+console.log(car !== "subaru");
+
+// test #6
+console.log("Is car.length > 3 ? I predict True.");
+console.log(car.length > 3);
+
+// test #7
+console.log("Is car.length == 6 ? I predict True.");
+console.log(car.length == 6);
+
+// test #8
+console.log("Is car.toUpperCase() === 'subaru' ? I predict False.");
+console.log(car.toUpperCase() === "subaru");
+
+// test #9
+console.log("Is car.tolowerCase() === 'subaru' ? I predict False.");
+console.log(car.toLowerCase() === "subaru");
+
+// test #10
+console.log("Is car.charAt(0) == 'a' ? I predict False.");
+console.log(car.charAt(0) == "a");
