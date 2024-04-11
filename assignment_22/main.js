ce3e27971743ba509ceeac0ef0f31fa788fa9a1f
MasoodNoor <themasoodnoor100@gmail.com>
2024-04-12 04:11:55 +0500

    Assignment 22 complete

---
+++ assignment_22/main.js
@@ -0,0 +1,4 @@
+"use strict";
+let myIntentionlerror = ["UK", "US", "UAE", "PK", "IN"];
+console.log("My intentional error: ", myIntentionlerror[5]);
+console.log("My intentional error resolved: ", myIntentionlerror[3]);
---
+++ assignment_22/main.ts
@@ -0,0 +1,4 @@
+let myIntentionlerror: string[] = ["UK", "US", "UAE", "PK", "IN"]
+console.log("My intentional error: ", myIntentionlerror[5]);
+console.log("My intentional error resolved: ", myIntentionlerror[3]);
+
