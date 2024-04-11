34bfa72e25ea19dc475ec674581fa3df69891e39
MasoodNoor <themasoodnoor100@gmail.com>
2024-04-12 01:20:41 +0500

    Assignment 19 complete

---
+++ assignment_19/main.js
@@ -0,0 +1,17 @@
+"use strict";
+let guestlist = ["Rameez", "Shahjahan", "Arshad", "Shazaib"];
+console.log("We regret to inform you that the new table wont be available. We only have limited seats available");
+// adding new list of guest
+guestlist.unshift("Masood", "Karar");
+console.log(`New guests in list: ${guestlist}`);
+// removing guest from list using splice function
+while (guestlist.length > 2) {
+    let removeGuest = guestlist.pop();
+    if (removeGuest !== undefined) {
+        console.log(`We apologise Mr.${removeGuest}, you are not invited to dinner`);
+    }
+}
+// printing last 2 guests
+guestlist.forEach(myguest => {
+    console.log(`Dear ${myguest}, you are invited to dinner`);
+});
