cc0b2c701acc68a13208e0624e900ac05229e45d
MasoodNoor <themasoodnoor100@gmail.com>
2024-04-12 03:55:59 +0500

    Assginment 21 complete

---
+++ assignment_21/main.js
@@ -0,0 +1,22 @@
+"use strict";
+let harddriveInStore = {
+    "NVMe SSD": {
+        name: "PCIe 4.0 NVMe SSD",
+        size: 250,
+        price: 3000,
+        company: "Kingston",
+    },
+    "SATA SSD": {
+        name: "A400 SATA SSD",
+        size: 500,
+        price: 1700,
+        company: "Kingston",
+    },
+    "HDD": {
+        name: "1TB HDD",
+        size: 1000,
+        price: 1000,
+        company: "Kingston",
+    }
+};
+console.log(harddriveInStore);
